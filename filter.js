b2966f1b37ab896871688d974aa58fc1adc4d938
cevznriny <57298997+cevznriny@users.noreply.github.com>
2023-04-17 17:13:45 +0200

    filter

---
+++ filter.js
@@ -0,0 +1,21 @@
+async function filter_figures()  {
+    const input = document.getElementById('filter').value;
+    console.log(input);
+    const spans = document.querySelectorAll('.en, .da, .la');
+    const re = new RegExp(input,'i');
+    const results  = document.getElementById('results');
+    results.innerHTML = '';
+    if (input == '') {
+        return;
+    }
+    spans.forEach(e => { 
+        //console.log(e.innerText);
+        if (e.innerText.match(re)) {
+            const figure = e.parentNode.parentNode;
+            const clone = figure.cloneNode(true);
+            results.appendChild(clone);
+        } else {
+
+        }
+        });
+    }
